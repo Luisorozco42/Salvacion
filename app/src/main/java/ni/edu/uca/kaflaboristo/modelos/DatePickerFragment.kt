b0f5f5d48f6906dbe11bcd18d8419098a89e7821
TorreManias <correosinnombre02@gmail.com>
2021-10-31 22:43:25 -0600

    Calendario corregido

--- app/src/main/java/ni/edu/uca/kaflaboristo/modelos/DatePickerFragment.kt
+++ app/src/main/java/ni/edu/uca/kaflaboristo/modelos/DatePickerFragment.kt
@@ -23,6 +23,8 @@ class DatePickerFragment (val listener: (day:Int, month:Int, year:Int) -> Unit) 
         val month = c.get(Calendar.MONTH)
         val day = c.get(Calendar.DAY_OF_MONTH)
         val picker = DatePickerDialog(activity as Context, R.style.DatePickerTheme, this, day, month, year)
+        c.add(Calendar.YEAR, -18)
+        picker.datePicker.maxDate = c.timeInMillis
         return picker
     }
 
